0aaf7e6ad04ae9c5f37843c97edbc05cbf58a6ac
emirsennin <n19238816@cs.hacettepe.edu.tr>
2020-05-03 14:17:01 +0300

    flight datas augmented to its final contitions.

--- preprocessing.py
+++ preprocessing.py
@@ -25,20 +25,20 @@ def resample_fixed(df, n_new):
     return pd.DataFrame(mat_new, index=x_new, columns=df.columns)
 
 def augmentTheData():
-    path = os.getcwd() + "/not augmented data"
-    all_files = glob.glob(path + "/*.xlsx")
+    path = os.getcwd() + "/Flight 10"
+    all_files = glob.glob(path + "/*.csv")
 
-    if not os.path.exists(os.getcwd() + "/augmented data"):
-        os.makedirs(os.getcwd() + "/augmented data")
+    if not os.path.exists(os.getcwd() + "/Flight 10 Augmented"):
+        os.makedirs(os.getcwd() + "/Flight 10 Augmented")
 
     for filename in all_files:
-        df = pd.read_excel(filename, index_col=None, header=0)
+        df = pd.read_csv(filename, index_col=None, header=0)
         resampledDf = resample_fixed(df, 5000)
         baseName = os.path.basename(filename)
-        resampledDf.to_excel(os.getcwd() + "/augmented data/" + baseName[:-5] + "augmented.xlsx", index=None)
+        resampledDf.to_excel(os.getcwd() + "/Flight 10 Augmented/" + baseName[:-5] + "augmented.xlsx", index=None)
 
 def fixTimestampsOfAugmentedFiles():
-    path = os.getcwd()+"/augmented data"
+    path = os.getcwd()+"/Flight 10 Augmented"
     all_files = glob.glob(path+"/*.xlsx")
 
     timestamps = pd.Series()
@@ -53,14 +53,35 @@ def fixTimestampsOfAugmentedFiles():
             df.to_excel(filename, index=None)
 
 
+def makeFinalCSVFiles(i):
+    path = os.getcwd()+"/Flight "+str(i)+" Augmented"
+    all_files = glob.glob(path+"/*.xlsx")
+
+    df = pd.read_excel(all_files[0],index_col=None,header=0)
+
+    for i in range(1,len(all_files)):
+        tempDf = pd.read_excel(all_files[i],index_col= None,header=0)
+        tempDf = tempDf.drop(['timestamp'],axis=1)
+        df = pd.concat([df, tempDf], axis=1)
+    return df
+
+
 if __name__ == "__main__":
-    filename = r"C:\Users\emir\PycharmProjects\ML-Flight_Maneuvers-Predictor\augmented data\actuator outputsaugmented.xlsx"
-    sns.set(rc={'figure.figsize': (20, 10)})
-    df = pd.read_excel(filename, index_col=None, header=0)
-    df.plot(x = "timestamp",y = ["output[2]","output[3]","output[4]","output[5]","output[6]","output[7]"])
-    plt.show()
+    # filename = r"C:\Users\emir\PycharmProjects\ML-Flight_Maneuvers-Predictor\augmented data\actuator outputsaugmented.xlsx"
+    # sns.set(rc={'figure.figsize': (20, 10)})
+    # df = pd.read_excel(filename, index_col=None, header=0)
+    # df.plot(x = "timestamp",y = ["output[2]","output[3]","output[4]","output[5]","output[6]","output[7]"])
+    # plt.show()
     # augmentTheData()
+    # print("augmentation finished")
     # fixTimestampsOfAugmentedFiles()
+    if not os.path.exists(os.getcwd() + "/Flights Final"):
+        os.makedirs(os.getcwd() + "/Flights Final")
+    path = os.getcwd()+"/Flights Final"
+    for i in range(1,11):
+        df = makeFinalCSVFiles(i)
+        # df.to_csv(path+"/flight_"+str(i)+"_augmented.csv",index=False)
+        df.to_excel(path+"/flight_"+str(i)+"_augmented.xlsx",index=False)
 
 
 # merged_dfs = pd.read_csv(all_files[0],index_col=None,header=0)
